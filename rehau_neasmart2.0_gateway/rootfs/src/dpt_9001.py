74c608ddc573d568c69442e675c548136050280f
Álvaro Najarro <najarro@users.noreply.github.com>
2026-07-14 10:32:53 +0200

    Update INVALID_DPT9001_REGISTERS with new value
    Added 0x8116 to the set of invalid DPT9001 registers.

--- rehau_neasmart2.0_gateway/rootfs/src/dpt_9001.py
+++ rehau_neasmart2.0_gateway/rootfs/src/dpt_9001.py
@@ -1,6 +1,8 @@
 import struct
 
-INVALID_DPT9001_REGISTERS = {0x7FFF}
+# REHAU uses 0x8116 for an inapplicable standby setpoint and 0x7FFF for
+# uninitialised temperature values. Neither is a real temperature.
+INVALID_DPT9001_REGISTERS = {0x7FFF, 0x8116}
 MIN_REASONABLE_TEMPERATURE = -50
 MAX_REASONABLE_TEMPERATURE = 120
 
